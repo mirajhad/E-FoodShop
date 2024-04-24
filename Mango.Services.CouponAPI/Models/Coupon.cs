8498a736a2ba43b16a6674823e273488b5679074
mirajhad <mirajhadish48@gmail.com>
2024-04-24 21:57:13 +0530

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
