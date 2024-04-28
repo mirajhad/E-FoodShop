62e1b4c3c583c754c40b720041bde73f6d10eaa0
mirajhad <mirajhadish48@gmail.com>
2024-04-28 13:33:59 +0530

    (feat): product crud

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -10,7 +10,7 @@ namespace Mango.Services.ProductAPI.Controllers
 {
     [Route("api/product")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class ProductAPIController : ControllerBase
     {
         private readonly AppDbContext _db;
@@ -58,7 +58,7 @@ namespace Mango.Services.ProductAPI.Controllers
       
 
         [HttpPost]
-      //  [Authorize(Roles = "ADMIN")]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Post([FromBody] ProductDto productDto)
         {
             try
@@ -77,7 +77,7 @@ namespace Mango.Services.ProductAPI.Controllers
         }
 
         [HttpPut]
-       // [Authorize(Roles = "ADMIN")]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Put([FromBody] ProductDto productDto)
         {
             try
@@ -97,7 +97,7 @@ namespace Mango.Services.ProductAPI.Controllers
 
         [HttpDelete]
         [Route("{id:int}")]
-       // [Authorize(Roles = "ADMIN")]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Delete(int id)
         {
             try
