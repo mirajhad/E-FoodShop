6594400839eadc6132f12a3271ffbeab260902d9
mirajhad <mirajhadish48@gmail.com>
2024-04-29 10:04:09 +0530

    (feat): dto in shopping cart

---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public Object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
