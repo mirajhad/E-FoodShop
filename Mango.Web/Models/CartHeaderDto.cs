93963f343b92d32cb56f56bd5d0465e611ed893f
mirajhad <mirajhadish48@gmail.com>
2024-04-30 21:45:04 +0530

    (feat): shopping cart web setup

---
+++ Mango.Web/Models/CartHeaderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Models
+{
+    public class CartHeaderDto
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        [NotMapped]
+        public double Discount { get; set; }
+        [NotMapped]
+        public double CartTotal { get; set; }
+    }
+}
---
+++ Mango.Web/Service/IService/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto?> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+    }
+}
