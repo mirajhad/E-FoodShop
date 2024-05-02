ce0dee6b2530bd09b0d0e39f8937900671db9f70
mirajhad <mirajhadish48@gmail.com>
2024-05-02 15:37:12 +0530

    (feat): Email API

---
+++ Mango.Services.EmailAPI/Models/Dto/CartHeaderDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.EmailAPI.Models.Dto
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+}
