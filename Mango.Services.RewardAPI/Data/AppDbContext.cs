4113e3a7a567d5f5422d82cbbc2908fa58880e3d
mirajhad <mirajhadish48@gmail.com>
2024-05-09 21:13:56 +0530

    setup

---
+++ Mango.Services.RewardAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.RewardAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Rewards> Rewards { get; set; }     
+    }
+}
---
+++ Mango.Services.RewardAPI/Models/Rewards.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.RewardAPI.Models
+{
+    public class Rewards
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime RewardsDate { get; set; }
+        public int RewardsActivity { get; set; }
+        public int OrderId { get; set; }
+    }
+}
